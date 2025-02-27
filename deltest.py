1e7db9277d4039cddb4a68b32dd3bc0d3502943f
VishalPokharel <077bct094.vishal@pcampus.edu.np>
2025-02-27 20:19:02 +0545

    added regextokenizer

--- deltest.py
+++ deltest.py
@@ -1,8 +1,8 @@
-from byte_pair_encoding import BasicTokenizer
-tokenizer=BasicTokenizer()
+from byte_pair_encoding import BasicTokenizer, RegexTokenizer
+tokenizer=RegexTokenizer()
 
 # A sample text to test encoding and decoding
-sample_text = "h"
+sample_text = "f <|endoftext|>"
 
 # Encode the sample text to a list of token IDs
 encoded_tokens = tokenizer.encode(sample_text)
